8bc58a767087daef8311dc90cc736ec959babbc5
ilhmmuhammadd <99084397+ilhmmuhammadd@users.noreply.github.com>
2022-02-06 21:10:24 +0700

    Add files via upload

---
+++ mael.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int l, s;
+
+  cout << "===============================\n";
+  cout << "Program Menghitung Luas Persegi\n";
+  cout << "===============================\n";
+
+  cout << "Masukkan panjang sisi persegi: ";
+  cin >> s;
+
+  l = s*s;
+
+  cout << "Luas persegi adalah "<< l << endl;
+
+  return 0;
+}
